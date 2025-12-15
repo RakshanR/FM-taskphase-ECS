8c1c2b82492efe234a82309a8a8185fbc2af9950
RakshanR <rakshanrajendran7@yahoo.com>
2025-12-15 14:54:10 +0530

    Update Q9.c

--- Q9.c
+++ Q9.c
@@ -1,16 +1,32 @@
 #include <stdio.h>
 int main() {
-    int n, i, j, space;
-    printf("Enter number of rows: ");
-    scanf("%d", &n);
-    for (i= 1; i<= n; i++) {
-        for (space= 1;space<= n- i; space++) {
-            printf(" ");
+    int matrix[4][4];
+    int totalSum = 0;
+    int prim= 0, sec= 0;
+    printf("Enter elements of 4x4 matrix:\n");
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 4; j++) {
+            scanf("%d", &matrix[i][j]);
         }
-        for (j= 1; j <= (2*i-1);j++) {
-            printf("*");
+    }
+    printf("\nMatrix is:\n");
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 4; j++) {
+            printf("%d\t", matrix[i][j]);
         }
         printf("\n");
     }
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 4; j++) {
+            totalSum += matrix[i][j];
+            if(i == j)              
+                prim+= matrix[i][j];
+            if(i + j == 3)          
+                sec+= matrix[i][j];
+        }
+    }
+    printf("\nSum of all elements: %d\n", totalSum);
+    printf("Sum of primary diagonal: %d\n", prim);
+    printf("Sum of secondary diagonal: %d\n", sec);
     return 0;
 }
