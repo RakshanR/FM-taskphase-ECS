514b3b0d30b516db0274a69043086713772231c1
RakshanR <rakshanrajendran7@yahoo.com>
2025-12-21 19:11:32 +0530

    Update Q3.c

--- Q3.c
+++ Q3.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
-int main() {
-    int units;
+int main(){
+    float units;
     float bill;
     printf("Enter total units: ");
-    scanf("%d",&units);
-    if (units<=100) {
+    if(scanf("%f",&units)!=1){
+        printf("Invalid input! Please enter a numeric value only.\n");
+        return 1;
+    }
+    if(units<0){
+        printf("Units cannot be negative.\n");
+        return 1;
+    }
+    if(units<=100){
         bill=units*5;
     }
-    else if (units<=300){
-        bill=(100*5)+ (units-100)*8;
+    else if(units<=300){
+        bill=(100*5)+(units-100)*8;
     }
-    else {
+    else{
         bill=(100*5)+(200*8)+(units-300)*10;
     }
-    printf("Total Electricity bill= rs.%.2f\n", bill);
+    printf("Total Electricity bill=rs.%.2f\n",bill);
     return 0;
+}
