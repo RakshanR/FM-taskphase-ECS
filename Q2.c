fbe9adc34e07cc21dee91e94dc02ab507f14436d
RakshanR <rakshanrajendran7@yahoo.com>
2025-12-21 19:10:51 +0530

    Update Q2.c

--- Q2.c
+++ Q2.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
-int main() {
-    int seconds, hrs, mins, rem_seconds;
+int main(){
+    float seconds;
+    int hrs,mins;
+    float rem_seconds;
     printf("Enter total seconds: ");
-    scanf("%d", &seconds);
-    hrs = seconds / 3600;
-    mins = (seconds % 3600) / 60;
-    rem_seconds = seconds % 60;
-    printf("Hours: %d\n", hrs);
-    printf("Minutes: %d\n", mins);
-    printf("Seconds: %d\n", rem_seconds);
+    if(scanf("%f",&seconds)!=1){
+        printf("Invalid input! Please enter a numeric value only.\n");
+        return 1;
+    }
+
+    if(seconds<0){
+        printf("Seconds cannot be negative.\n");
+        return 1;
+    }
+    hrs=(int)(seconds/3600);
+    mins=(int)((seconds-(hrs*3600))/60);
+    rem_seconds=seconds-(hrs*3600)-(mins*60);
+
+    printf("Hours:%d\n",hrs);
+    printf("Minutes:%d\n",mins);
+    printf("Seconds:%.2f\n",rem_seconds);
+
     return 0;
 }
