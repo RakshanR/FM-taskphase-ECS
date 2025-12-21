c4b195ecaf24a33337a0993dce98ac207a4203a4
RakshanR <rakshanrajendran7@yahoo.com>
2025-12-21 19:12:04 +0530

    Update Q4.c

--- Q4.c
+++ Q4.c
@@ -1,22 +1,30 @@
-#include <stdio.h>
-#include <string.h>
-int main() {
-    char pwd[20];
-    char correct_pwd[] = "rakshan";
-    int attempts = 3;
-    while (attempts > 0) {
+int main(){
+    char pwd[30];
+    char correct_pwd[]="rakshan";
+    int attempts=3;
+    while(attempts>0){
         printf("Enter password: ");
-        scanf("%s", pwd);
-        if (strcmp(pwd, correct_pwd) == 0) {
+        if(scanf("%29s",pwd)!=1){
+            printf("Invalid input\n");
+            return 1;
+        }
+        if(strchr(pwd,' ')!=NULL){
+            printf("Spaces are not allowed in password\n");
+            continue;
+        }
+        if(strlen(pwd)==0){
+            printf("Password cannot be empty\n");
+            continue;
+        }
+        if(strcmp(pwd,correct_pwd)==0){
             printf("Login successful\n");
             return 0;
-        } else {
+        }else{
             attempts--;
             printf("Incorrect password\n");
         }
     }
     printf("Too many failed attempts\n");
-
     return 0;
 }
 
